dea7d39e7f968d2181882438876a13a218d4bd7a
Lisa Ross <lr@footprintleadership.com>
2018-08-31 08:57:05 -0400

    :art: image and ga sw
    testing different caches

---
+++ playground.js
@@ -0,0 +1,9 @@
+var h1 = document.createElement("h1");
+h1.innerHTML = "playground in the house!";
+
+var kittenImg = document.createElement("img");
+kittenImg.src = "./500.jpg";
+
+var playground = document.getElementById("root");
+playground.appendChild(h1);
+playground.appendChild(kittenImg);
--- sw.js
+++ sw.js
@@ -1,1 +1,68 @@
-console.log("Hello from sw.js");
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js"
+);
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.googleAnalytics.initialize();
+
+if ("storage" in navigator && "estimate" in navigator.storage) {
+  navigator.storage.estimate().then(({ usage, quota }) => {
+    console.log(`Using ${usage} out of ${quota} bytes.`);
+  });
+}
+
+workbox.routing.registerRoute(
+  new RegExp(".*.js"),
+  workbox.strategies.networkFirst({
+    cacheName: "js-cache"
+  })
+);
+workbox.routing.registerRoute(
+  // Cache CSS files
+  /.*\.css/,
+  // Use cache but update in the background ASAP
+  workbox.strategies.staleWhileRevalidate({
+    // Use a custom cache name
+    cacheName: "css-cache"
+  })
+);
+
+workbox.routing.registerRoute(
+  // Cache image files
+  /.*\.(?:png|jpg|jpeg|svg|gif)/,
+  // Use the cache if it's available
+  workbox.strategies.cacheFirst({
+    // Use a custom cache name
+    cacheName: "image-cache",
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Cache only 20 images
+        maxEntries: 20,
+        // Cache for a maximum of a week
+        maxAgeSeconds: 7 * 24 * 60 * 60
+      })
+    ]
+  })
+);
+
+// Cache the Google Fonts webfont files with a cache first strategy for 1 year.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  workbox.strategies.cacheFirst({
+    cacheName: "google-fonts-webfonts",
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30
+      })
+    ]
+  })
+);
